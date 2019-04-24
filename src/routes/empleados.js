8da8043510415e027350a0ee46c6a91b09a0b5b9
Erick Lugo <ericklugoj@gmail.com>
2019-04-24 03:04:54 -0500

    Se agrego el formulario para agregar empleado, vistas para listar empleados, se arreglaron links del menu para empleados, se agrego color al logo, se estilizo el menu de nav

---
+++ src/routes/empleados.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const router = express.Router();
+
+const pool = require('../database');
+const {
+    isLoggedIn
+} = require('../lib/auth');
+
+router.get('/', isLoggedIn, async (req, res) => {
+    const links = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id]);
+    res.render('empleados/listar', {
+        links
+    });
+});
+
+router.get('/agregar', isLoggedIn, (req, res) => {
+    res.render('empleados/agregar');
+});
+
+router.post('/add', isLoggedIn, async (req, res) => {
+    const {
+        title,
+        url,
+        description
+    } = req.body;
+
+    const newLink = {
+        title,
+        url,
+        description,
+        user_id: req.user.id
+    };
+
+    await pool.query('INSERT INTO links SET ?', [newLink]);
+    //mensajes
+    req.flash('success', 'Link agregado');
+    res.redirect('/links');
+});
+
+router.get('/', isLoggedIn, async (req, res) => {
+    const links = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id]);
+    res.render('links/list', {
+        links
+    });
+});
+
+router.get('/delete/:id', isLoggedIn, async (req, res) => {
+    const {
+        id
+    } = req.params;
+
+    pool.query('DELETE FROM links WHERE id = ?', [id]);
+    req.flash('success', 'Link eliminado');
+    res.redirect('/links');
+});
+
+router.get('/edit/:id', isLoggedIn, async (req, res) => {
+    const {
+        id
+    } = req.params;
+
+    const links = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
+
+    res.render('links/edit', {
+        link: links[0]
+    });
+});
+
+router.post('/edit/:id', isLoggedIn, async (req, res) => {
+    const {
+        id
+    } = req.params;
+
+    const {
+        title,
+        description,
+        url
+    } = req.body;
+
+    const newLink = {
+        title,
+        description,
+        url
+    };
+
+    await pool.query('UPDATE links SET ? WHERE id = ?', [newLink, id]);
+
+    req.flash('success', 'Link actualizado');
+    res.redirect('/links');
+});
+
+module.exports = router;
