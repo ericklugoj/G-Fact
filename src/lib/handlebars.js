ba9571d1a956cf04369d3f994d076ea60f15e409
Erick Lugo <ericklugoj@gmail.com>
2019-04-28 19:16:20 -0500

    Listado y registro de empleados listo, trabajando en edicion de empleados

--- src/lib/handlebars.js
+++ src/lib/handlebars.js
@@ -2,10 +2,18 @@ const {
     format
 } = require('timeago.js');
 
+const {
+    date
+} = require('handlebars-helpers');
+
 const helpers = {};
 
 helpers.timeago = (timestamp) => {
     return format(timestamp);
+};
+
+helpers.formatter = (fecha) => {
+    return date(fecha);
 };
 
 module.exports = helpers;
